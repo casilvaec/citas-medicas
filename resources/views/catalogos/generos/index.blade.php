5d5d765db13eb77cfa376d5e7da78f6d88ba3a4a
Carlos Silva <casilva2@utpl.edu.ec>
2024-07-18 03:49:14 -0500

    CRUD de géneros funciona ok

--- resources/views/catalogos/generos/index.blade.php
+++ resources/views/catalogos/generos/index.blade.php
@@ -1,15 +1,10 @@
+<!-- resources/views/catalogos/generos/index.blade.php -->
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <h1>Géneros</h1>
     <a href="{{ route('admin.generos.create') }}" class="btn btn-primary">Agregar Género</a>
-
-    @if(session('success'))
-        <div class="alert alert-success mt-2">
-            {{ session('success') }}
-        </div>
-    @endif
 
     <table class="table mt-4">
         <thead>
