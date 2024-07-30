9357bfbb9f13b2abdff9117935a51ddf9abc69d0
Carlos Silva <casilva2@utpl.edu.ec>
2024-07-29 22:56:25 -0500

    Agendar cita médica funciona 90% falta arreglar boton Agendar Cita

--- app/Models/Cita.php
+++ app/Models/Cita.php
@@ -1,5 +1,42 @@
 <?php
 
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+// class Cita extends Model
+// {
+//     use HasFactory;
+
+//     protected $fillable = [
+//         'paciente_id',
+//         'medico_id',
+//         'especialidad_id',
+//         'fecha',
+//         'hora_inicio',
+//         'hora_fin',
+//         'motivo',
+//         'estado',
+//     ];
+
+//     public function paciente()
+//     {
+//         return $this->belongsTo(User::class, 'paciente_id');
+//     }
+
+//     public function medico()
+//     {
+//         return $this->belongsTo(Medico::class, 'medico_id');
+//     }
+
+//     public function especialidad()
+//     {
+//         return $this->belongsTo(EspecialidadesMedicas::class, 'especialidad_id');
+//     }
+// } -->
+
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,25 +46,20 @@ class Cita extends Model
 {
     use HasFactory;
 
+    protected $table = 'citas';
+
     protected $fillable = [
-        'paciente_id',
-        'medico_id',
-        'especialidad_id',
-        'fecha',
-        'hora_inicio',
-        'hora_fin',
-        'motivo',
-        'estado',
+        'pacienteId', 'medicoId', 'especialidad_id', 'fecha', 'hora_inicio', 'hora_fin', 'estado'
     ];
 
     public function paciente()
     {
-        return $this->belongsTo(User::class, 'paciente_id');
+        return $this->belongsTo(User::class, 'pacienteId');
     }
 
     public function medico()
     {
-        return $this->belongsTo(Medico::class, 'medico_id');
+        return $this->belongsTo(Medico::class, 'medicoId');
     }
 
     public function especialidad()
